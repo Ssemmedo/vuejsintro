9131017b8d5e91465297058589c74f777becabc4
Sandro Semedo <sandro.semedo@alunos.unis.edu.br>
2021-09-16 14:57:00 -0300

    Realizando o 2º exercício

--- vue.js
+++ vue.js
@@ -1,6 +1,13 @@
-var app = new Vue({
+var app1 = new Vue({
     el: '#app1',
     data: {
       message: 'Primeira vez programando em Vue!'
+    }
+  })
+
+  var app2 = new Vue({
+    el: '#app2',
+    data: {
+      message: 'Você está acessando essa página no dia: ' + new Date().toLocaleString()
     }
   })
